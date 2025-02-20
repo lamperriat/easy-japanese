2da609748e86f4f454a70206dd1dccc2066ec339
QizhenSun <qz_sun@sjtu.edu.cn>
2025-02-20 19:47:46 +0800

    feat: reading material apis finished

--- backend/pkg/handlers/globalReadingMaterialEditor.go
+++ backend/pkg/handlers/globalReadingMaterialEditor.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"backend/pkg/models"
 	"errors"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -68,4 +69,94 @@ func (h* WordHandler) EditReadingMaterial(c *gin.Context) {
 		"data": updatedReadingMaterial, 
 		"message": "Reading material updated",
 	})
+}
+
+func (h* WordHandler) DeleteReadingMaterial(c *gin.Context) {
+	var toDelete models.ReadingMaterial
+
+	if err := c.ShouldBindJSON(&toDelete); err != nil {
+		c.AbortWithStatusJSON(400, gin.H{"error": "Invalid JSON format"})
+		return
+	}
+
+	err := h.db.Transaction(func(tx *gorm.DB) error {
+		var existing models.ReadingMaterial
+		if err := tx.First(&existing, toDelete.ID).Error; err != nil {
+			return err
+		}
+		if err := tx.Delete(&toDelete).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, gin.H{"error": "Reading material not found"})
+		} else {
+			c.JSON(500, gin.H{"error": "Database error"})
+		}
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Reading material deleted",
+	})
+}
+
+const DefaultResultPerPage = 20
+
+func (h* WordHandler) GetReadingMaterials(c *gin.Context) {
+	page := c.Query("page")
+	resultPerPageStr := c.Query("RPP")
+	var pageInt int
+	var err error
+	pageInt, err = strconv.Atoi(page)
+	if err != nil || pageInt < 1 {
+		pageInt = 1
+	}
+	var resultPerPage int
+	resultPerPage, err = strconv.Atoi(resultPerPageStr)
+	if err != nil || resultPerPage < 1 || resultPerPage > 100 {
+		resultPerPage = DefaultResultPerPage
+	}
+	
+	var readingMaterials []models.ReadingMaterial
+	if err := h.db.
+		Limit(resultPerPage).
+		Offset((pageInt - 1) * resultPerPage).
+		Find(&readingMaterials).Error; err != nil {
+		c.JSON(500, gin.H{"error": "Database error"})
+		return
+	}
+
+	c.JSON(200, gin.H{"readingMaterials": readingMaterials})
+}
+
+func (h* WordHandler) FuzzySearchReadingMaterials(c *gin.Context) {
+	query := c.Query("query")
+	page := c.Query("page")
+	resultPerPageStr := c.Query("RPP")
+	var pageInt int
+	var err error
+	pageInt, err = strconv.Atoi(page)
+	if err != nil || pageInt < 1 {
+		pageInt = 1
+	}
+	var resultPerPage int
+	resultPerPage, err = strconv.Atoi(resultPerPageStr)
+	if err != nil || resultPerPage < 1 || resultPerPage > 100 {
+		resultPerPage = DefaultResultPerPage
+	}
+	var readingMaterials []models.ReadingMaterial
+	if err := h.db.
+		Where("content LIKE ?", "%"+query+"%").
+		Limit(resultPerPage).
+		Offset((pageInt - 1) * resultPerPage).
+		Find(&readingMaterials).Error; err != nil {
+		c.JSON(500, gin.H{"error": "Database error"})
+		return
+	}
+
+	c.JSON(200, gin.H{"readingMaterials": readingMaterials})
 }
