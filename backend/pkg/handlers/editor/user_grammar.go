67b099624d35cc0c84092d6a5dce6f6e7123b275
QizhenSun <qz_sun@sjtu.edu.cn>
2025-03-15 19:38:33 +0800

    feat(user): edit and delete user grammar

--- backend/pkg/handlers/editor/user_grammar.go
+++ backend/pkg/handlers/editor/user_grammar.go
@@ -56,3 +56,129 @@ func (h* WordHandler) AddGrammarUser(c *gin.Context) {
 
 	c.JSON(201, models.SuccessMsg{Message: "Grammar added"})
 }
+
+// @Summary Edit a grammar in user's dictionary
+// @Description 
+// @Tags userDictOp
+// @Security APIKeyAuth
+// @Accept json
+// @Produce json
+// @Success 200 {object} models.SuccessMsg
+// @Failure 400 {object} models.ErrorMsg "Invalid JSON"
+// @Failure 404 {object} models.ErrorMsg "User/Grammar not found"
+// @Failure 500 {object} models.ErrorMsg "Database error"
+// @Router /api/user/grammar/edit [post]
+func (h* WordHandler) EditGrammarUser(c *gin.Context) {
+	providedKey := c.GetHeader("X-API-Key")
+	keyhash := auth.Sha256hex(providedKey)
+	var user models.User
+	if err := h.db.Where("keyhash =?", keyhash).
+		First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "User not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		} 
+		return
+	}
+	var editedGrammar models.UserGrammar
+	if err := c.ShouldBindJSON(&editedGrammar); err != nil {
+		c.AbortWithStatusJSON(400, models.ErrorMsg{Error: "Invalid JSON"})
+		return
+	}
+	err := h.db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&models.UserGrammar{}).
+			Where("id = ?", editedGrammar.ID).
+			Omit("Examples").
+			Updates(&editedGrammar).Error; err != nil {
+			return err
+		}
+
+		editedGrammar.UserID = user.ID
+		if err := tx.Where("grammar_id = ?", editedGrammar.ID).
+			Delete(&models.UserGrammarExample{}).Error; err != nil {
+			return nil
+		}
+
+		if len(editedGrammar.Examples) > 0 {
+			for i := range editedGrammar.Examples {
+				editedGrammar.Examples[i].GrammarID = editedGrammar.ID
+				editedGrammar.Examples[i].ID = 0
+			}
+			if err := tx.Create(&editedGrammar.Examples).Error; err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "Grammar not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		}
+		return
+	}
+
+	c.JSON(200, models.SuccessMsg{Message: "Grammar edited"})
+}
+
+
+// @Summary Delete a grammar from user's dictionary
+// @Description 
+// @Tags userDictOp
+// @Security APIKeyAuth
+// @Accept json
+// @Produce json
+// @Success 200 {object} models.SuccessMsg
+// @Failure 400 {object} models.ErrorMsg "Invalid JSON"
+// @Failure 404 {object} models.ErrorMsg "User/Grammar not found"
+// @Failure 500 {object} models.ErrorMsg "Database error"
+// @Router /api/user/grammar/delete [post]
+func (h* WordHandler) DeleteGrammarUser(c *gin.Context) {
+	providedKey := c.GetHeader("X-API-Key")
+	keyhash := auth.Sha256hex(providedKey)
+	var user models.User
+	if err := h.db.Where("keyhash =?", keyhash).
+		First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "User not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		} 
+		return
+	}
+	var toDelete models.UserGrammar
+	if err := c.ShouldBindJSON(&toDelete); err != nil {
+		c.AbortWithStatusJSON(400, models.ErrorMsg{Error: "Invalid JSON"})
+		return
+	}
+	toDelete.UserID = user.ID
+	err := h.db.Transaction(func(tx *gorm.DB) error {
+		var existing models.UserGrammar
+		if err := tx.First(&existing, toDelete.ID).Error; err != nil {
+			return err
+		}
+		if err := tx.Where("grammar_id = ?", toDelete.ID).
+			Delete(&models.UserGrammarExample{}).
+			Error; err != nil {
+			return err
+		}
+		if err := tx.Delete(&toDelete).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "Grammar not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		}
+		return
+	}
+
+	c.JSON(200, models.SuccessMsg{Message: "Grammar deleted"})
+}
