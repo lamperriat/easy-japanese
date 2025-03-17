fb270ae99460f10dfa89ca8b049ff16446d99b4e
QizhenSun <qz_sun@sjtu.edu.cn>
2025-03-17 19:38:35 +0800

    feat(user): edit and delete reading

--- backend/pkg/handlers/editor/user_reading.go
+++ backend/pkg/handlers/editor/user_reading.go
@@ -53,3 +53,108 @@ func (h* WordHandler) AddReadingMaterialUser(c *gin.Context) {
 	}
 	c.JSON(201, models.SuccessMsg{Message: "Reading material added"})
 }
+
+// @Summary Edit a reading material in user's dictionary
+// @Description
+// @Tags userDictOp
+// @Security APIKeyAuth
+// @Accept json
+// @Produce json
+// @Success 200 {object} models.SuccessMsg
+// @Failure 400 {object} models.ErrorMsg "Invalid JSON"
+// @Failure 404 {object} models.ErrorMsg "User not found"
+// @Failure 500 {object} models.ErrorMsg "Database error"
+// @Router /api/user/reading-material/edit [post]
+func (h* WordHandler) EditReadingMaterialUser(c *gin.Context) {
+	providedKey := c.GetHeader("X-API-Key")
+	keyhash := auth.Sha256hex(providedKey)
+	var user models.User
+	if err := h.db.Where("keyhash = ?", keyhash).
+		First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "User not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		}
+		return
+	}
+	var updatedReadingMaterial models.UserReadingMaterial
+	if err := c.ShouldBindJSON(&updatedReadingMaterial); err != nil {
+		c.AbortWithStatusJSON(400, models.ErrorMsg{Error: "Invalid JSON"})
+		return
+	}
+	updatedReadingMaterial.UserID = user.ID
+	err := h.db.Transaction(func(tx *gorm.DB) error {
+		var existing models.UserReadingMaterial
+		if err := tx.First(&existing, updatedReadingMaterial.ID).Error; err != nil {
+			return err
+		}
+		if err := tx.Model(&existing).
+			Updates(&updatedReadingMaterial).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "Reading material not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		}
+		return
+	}
+
+	c.JSON(200, models.SuccessMsg{Message: "Reading material updated"})
+}
+
+// @Summary Delete a reading material from user's dictionary
+// @Description
+// @Tags userDictOp
+// @Security APIKeyAuth
+// @Accept json
+// @Produce json
+// @Success 200 {object} models.SuccessMsg
+// @Failure 400 {object} models.ErrorMsg "Invalid JSON"
+// @Failure 404 {object} models.ErrorMsg "User not found"
+// @Failure 500 {object} models.ErrorMsg "Database error"
+// @Router /api/user/reading-material/delete [post]
+func (h* WordHandler) DeleteReadingMaterialUser(c *gin.Context) {
+	providedKey := c.GetHeader("X-API-Key")
+	keyhash := auth.Sha256hex(providedKey)
+	var user models.User
+	if err := h.db.Where("keyhash = ?", keyhash).
+		First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "User not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		}
+		return
+	}
+	var toDelete models.UserReadingMaterial
+	if err := c.ShouldBindJSON(&toDelete); err != nil {
+		c.AbortWithStatusJSON(400, models.ErrorMsg{Error: "Invalid JSON"})
+		return
+	}
+	toDelete.UserID = user.ID
+	err := h.db.Transaction(func(tx *gorm.DB) error {
+		var existing models.UserReadingMaterial
+		if err := tx.First(&existing, toDelete.ID).Error; err != nil {
+			return err
+		}
+		if err := tx.Delete(&toDelete).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "Reading material not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		}
+		return
+	}
+	c.JSON(200, models.SuccessMsg{Message: "Reading material deleted"})
+}
