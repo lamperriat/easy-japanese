ede5358a43184d02624df8e7f09a88c0a84b9bc6
QizhenSun <qz_sun@sjtu.edu.cn>
2025-03-17 19:44:09 +0800

    feat(user): get and fuzzy search in readings

--- backend/pkg/handlers/editor/user_reading.go
+++ backend/pkg/handlers/editor/user_reading.go
@@ -4,6 +4,7 @@ import (
 	"backend/pkg/auth"
 	"backend/pkg/models"
 	"errors"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -157,4 +158,122 @@ func (h* WordHandler) DeleteReadingMaterialUser(c *gin.Context) {
 		return
 	}
 	c.JSON(200, models.SuccessMsg{Message: "Reading material deleted"})
+}
+
+// @Summary Browse all reading materials from user's dictionary
+// @Description
+// @Tags userDictOp
+// @Security APIKeyAuth
+// @Accept json
+// @Produce json
+// @Param page query int false "Page number"
+// @Param RPP query int false "Results per page"
+// @Success 200 {object} []models.UserReadingMaterial
+// @Failure 404 {object} models.ErrorMsg "User not found"
+// @Failure 500 {object} models.ErrorMsg "Database error"
+// @Router /api/user/reading-material/get [get]
+func (h* WordHandler) GetReadingMaterialUser(c *gin.Context) {
+	providedKey := c.GetHeader("X-API-Key")
+	keyhash := auth.Sha256hex(providedKey)
+	var user models.User
+	if err := h.db.Where("keyhash = ?", keyhash).
+		First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "User not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		}
+		return
+	}
+	page := c.Query("page")
+	resultPerPageStr := c.Query("RPP")
+	var pageInt int
+	var err error
+	pageInt, err = strconv.Atoi(page)
+	if err != nil || pageInt < 1 {
+		pageInt = 1
+	}
+	var resultPerPage int
+	resultPerPage, err = strconv.Atoi(resultPerPageStr)
+	if err != nil || resultPerPage < 1 {
+		resultPerPage = defaultResultPerPage
+	}
+
+	var readingMaterials []models.UserReadingMaterial
+	if err := h.db.
+		Where("user_id = ?", user.ID).
+		Limit(resultPerPage).
+		Offset((pageInt - 1) * resultPerPage).
+		Find(&readingMaterials).Error; err != nil {
+		c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		return
+	}
+	c.JSON(200, readingMaterials)
+}
+
+// @Summary Fuzzy search in all reading materials from user's dictionary
+// @Description
+// @Tags userDictOp
+// @Security APIKeyAuth
+// @Accept json
+// @Produce json
+// @Param page query int false "Page number"
+// @Param RPP query int false "Results per page"
+// @Param query query string true "Search query"
+// @Success 200 {object} models.SearchResult[models.UserReadingMaterial]
+// @Failure 404 {object} models.ErrorMsg "User not found"
+// @Failure 500 {object} models.ErrorMsg "Database error"
+// @Router /api/user/reading-material/search [get]
+func (h* WordHandler) FuzzySearchReadingMaterialUser(c *gin.Context) {
+	providedKey := c.GetHeader("X-API-Key")
+	keyhash := auth.Sha256hex(providedKey)
+	var user models.User
+	if err := h.db.Where("keyhash = ?", keyhash).
+		First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, models.ErrorMsg{Error: "User not found"})
+		} else {
+			c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		}
+		return
+	}
+	page := c.Query("page")
+	resultPerPageStr := c.Query("RPP")
+	var pageInt int
+	var err error
+	pageInt, err = strconv.Atoi(page)
+	if err != nil || pageInt < 1 {
+		pageInt = 1
+	}
+	var resultPerPage int
+	resultPerPage, err = strconv.Atoi(resultPerPageStr)
+	if err != nil || resultPerPage < 1 {
+		resultPerPage = defaultResultPerPage
+	}
+	query := c.Query("query")
+	var readingMaterials []models.UserReadingMaterial
+	var count int64
+	if err := h.db.
+		Model(&models.UserReadingMaterial{}).
+		Where("user_id = ? AND (title LIKE ? OR content LIKE ?)",
+			user.ID, "%"+query+"%", "%"+query+"%").
+		Count(&count).Error; err != nil {
+		c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		return
+	}
+	if err := h.db.
+		Where("user_id = ? AND (title LIKE ? OR content LIKE ?)",
+			user.ID, "%"+query+"%", "%"+query+"%").
+		Limit(resultPerPage).
+		Offset((pageInt - 1) * resultPerPage).
+		Find(&readingMaterials).Error; err != nil {
+		c.JSON(500, models.ErrorMsg{Error: "Database error"})
+		return
+	}
+	c.JSON(200, models.SearchResult[models.UserReadingMaterial]{
+		Count:    count, 
+		Page:     pageInt,
+		PageSize: resultPerPage,
+		Results:  readingMaterials,
+	})
 }
