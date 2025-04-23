8cc00691402d11b0b121027e0f6b1ee05dd567a1
QizhenSun <qz_sun@sjtu.edu.cn>
2025-04-23 17:57:44 +0800

    feat: add about-us to navigation

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -103,6 +103,7 @@ export default function Navigation() {
           <li><Link to="/word-search"> 词库搜索</Link></li>
           <li><Link to="/review">复习</Link></li>
           <li><Link to="/"> 返回主页</Link></li>
+          <li><Link to="/About">关于</Link> </li>
           <li className="right-item">
             <button 
               className="username-btn" 
